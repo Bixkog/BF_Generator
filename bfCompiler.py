516966344036cd40c62d121834192f879fe73926
wiatrak2 <wpratkowiecki@gmail.com>
2018-01-17 22:21:28 +0100

    added unmatched braces in BF code handling

--- bfCompiler.py
+++ bfCompiler.py
@@ -4,27 +4,40 @@ def parse(code):
 
 def buildbracemap(code):
     bracestack, bracemap = [], {}
-
+    unmatched_braces, unmatched_braces_pos = 0, []
     for position, command in enumerate(code):
         if command == "[":
             bracestack.append(position)
         if command == "]":
-            start = bracestack.pop()
-            bracemap[start] = position
-            bracemap[position] = start
-    return bracemap
+            if bracestack == []:
+                unmatched_braces += 1
+                unmatched_braces_pos.append(position)
+            else:
+                start = bracestack.pop()
+                bracemap[start] = position
+                bracemap[position] = start
+    unmatched_braces += len(bracestack)
+    unmatched_braces_pos += bracestack
+    for i in xrange(len(unmatched_braces_pos)):
+        position = unmatched_braces_pos[i]
+        code = code[:(position - i)] + code[(position - i + 1):]
+    return code, bracemap, unmatched_braces
 
 
-def bfEvaluate(code, input_list, bracemap):
+def bfEvaluate(code, input_list, bracemap, ops_limit, max_ops):
     output = ''
     code_ptr = cell_ptr = input_ptr = 0
 
     cells_num = 3000
     cells = [0] * cells_num
 
+    operations_counter = 0
+
     while code_ptr < len(code):
         command = code[code_ptr]
-
+        if ops_limit and operations_counter > max_ops:
+            return None, max_ops
+        operations_counter += 1
         if command == ">":
             cell_ptr = cell_ptr + 1 if cell_ptr + 1 < cells_num else 0
 
@@ -52,19 +65,24 @@ def bfEvaluate(code, input_list, bracemap):
 
         code_ptr += 1
 
-    return output
+    return output, operations_counter
 
-def BF(code, input_list=''):
+
+def BF(code, input_list='', ops_limit=False, max_ops=5000):
     '''
         compiler of BF code
         Args:
             code (string): BF code
             input_list (string): input assigned to code (if needed)
+            ops_limit (bool): to prevent infinite loops you can return None after max_ops steps (default: False)
+            max_ops (int): if ops_limit is set to true determines the maximum number of operations (default: 5000)
         Returns:
-            string with output for given code
+            output: string with output for given code (may be None if ops_limit is true)
+            opertations_counter: number of program operations
+            unmatched_braces: number of unmatched braces (used in strict mode)
     '''
     code = parse(code)
-    bracemap = buildbracemap(code)
-    output = bfEvaluate(code, input_list, bracemap)
-    return output
-  
+    code, bracemap, unmatched_braces = buildbracemap(code)
+    output, operations_counter = bfEvaluate(
+        code, input_list, bracemap, ops_limit, max_ops)
+    return output, operations_counter, unmatched_braces
